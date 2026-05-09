6ccad89061d7b7a8ef8a8004a8de51c2d521f40a
EduardoBerton <eduardoberton01@gmail.com>
2026-05-09 08:04:26 -0300

    alteracoes

--- Exercícios/ex1117bc.py
+++ Exercícios/ex1117bc.py
@@ -1,14 +1,14 @@
 #Exercício 1117 - Validação de Notas
 
-nota1 = float(input("Nota 1: "))
+nota1 = float(input("Nota 1: ").replace(",","."))
 while(nota1 < 0 or nota1 > 10):
     print("Nota inválida.")
-    nota1 = float(input("Nota 1: "))
+    nota1 = float(input("Nota 1: ").replace(",","."))
 
-nota2 = float(input("Nota 2: "))
+nota2 = float(input("Nota 2: ").replace(",","."))
 while(nota2 < 0 or nota2 > 10):
     print("Nota inválida.") 
-    nota2 = float(input("Nota 2: "))  
+    nota2 = float(input("Nota 2: ").replace(",","."))  
 
 media = (nota1 + nota2) / 2 
 print(f"Média: {media}")     
--- Exercícios/exe05.py
+++ Exercícios/exe05.py
@@ -2,7 +2,7 @@ p1 = float(input("Nota da P1: "))
 p2 = float(input("Nota da p2: "))
 
 med = (p1 + p2)/2
-    
+
 if (med >= 6): 
     print("Média: %.2f. Aprovado!" % med)
 elif (5 <= med < 6): 
