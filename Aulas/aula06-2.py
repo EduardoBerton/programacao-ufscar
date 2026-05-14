48a2a9a6341954634ec909c1af5075d4450cec4e
EduardoBerton <eduardoberton01@gmail.com>
2026-05-13 21:02:08 -0300

    atualização

---
+++ Aulas/aula06-2.py
@@ -0,0 +1,7 @@
+#Exercicio 1075
+
+N = int(input())
+I = 0
+rest = 0
+for I in range (1, 10001, 1):
+    rest = N/I
---
+++ Aulas/aula06.py
@@ -0,0 +1,8 @@
+#Construir uma calculadora com as seguintes condições: 
+##Entregue todas as operações (1,10)(+,-,*,/)(1,10)
+##Apresente "ENTRADA INVALIDA" se algo diferente das operações for digitado
+##Encerre se for digitado "0"
+
+
+    
+                
